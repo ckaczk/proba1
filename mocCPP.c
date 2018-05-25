182c13f4921d339faf5aa243c050f1260c1ca20e
ckaczk <c.kaczkowski@hotmail.com>
2018-05-25 16:13:06 -0400

    mojCOnflikt

--- mocCPP.c
+++ mocCPP.c
@@ -1,4 +1,11 @@
 Pierwsza linijka
 druga linia
 trzecia linia
+<<<<<<< HEAD
 czwarta linia
+||||||| 960e247... wyjechala trzecia droga
+czwarta linia
+piata nowa droga trzecia 
+=======
+czwarta linia
+>>>>>>> parent of 960e247... wyjechala trzecia droga
